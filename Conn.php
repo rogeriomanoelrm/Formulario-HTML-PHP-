3025ae13860b1d240ad34b2b8e81717ad05e4681
rogeriomanoelrm <77088268+rogeriomanoelrm@users.noreply.github.com>
2023-04-21 01:55:24 -0300

    Update Conn.php

--- Conn.php
+++ Conn.php
@@ -4,26 +4,22 @@ abstract class Conn
 {
     public string $db = "mysql";
     public string $host = "localhost";
-    public string $user = "root";
-    public string $pass = "";
-    public string $dbname = "roger3";
+    public string $user = "u986902309_roger1";
+    public string $pass = "Roger*2023";
+    public string $dbname = "u986902309_roger";
     public int $port = 3306;
     public object $connect;
 
     public function connectDb()
     {
         try{
-            //Conexao com a porta
-            //$this->connect = new PDO($this->db . ':host=' . $this->host . ';port=' . $this->port . ';dbname='. $this->dbname, $this->user, $this->pass);
             
-            //Conexao sem a porta
+            
             $this->connect = new PDO($this->db . ':host=' . $this->host . ';dbname='. $this->dbname, $this->user, $this->pass);
             
-            //echo "Conexão com banco de dados realizado com sucesso!<br>";
             return $this->connect;
         }catch (Exception $err){
-            die('Erro: Por favor tente novamente. Caso o problema persista, entre em contato o administrador adm@empresa.com');
-            //echo "Erro: Conexão com banco de dados não realizado com sucesso! Erro gerado: " . $err->getMessage();
+           
         }
     }
 }
